c72f65b03e29ffc3560ce3bc4ca9fba228d4561f
J. Gonzalo Linares Paulet <gonzalolinares184@gmail.com>
2020-08-31 11:12:37 -0500

    Updates

---
+++ precache-manifest.7da56c1b7510fb5bdd7c7ece3e07938c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "656d18bc9abbb31d2f4513753870c4fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f0fc081738b4b1e8d8f",
+    "url": "/static/css/main.2fb68c4b.chunk.css"
+  },
+  {
+    "revision": "9654019c23d027d1a0be",
+    "url": "/static/js/2.b0aa026c.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/static/js/2.b0aa026c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f0fc081738b4b1e8d8f",
+    "url": "/static/js/main.97461837.chunk.js"
+  },
+  {
+    "revision": "8798da633dfe4d89d732",
+    "url": "/static/js/runtime-main.6ff56d75.js"
+  },
+  {
+    "revision": "8c4596ac1eb31fef653d66b5aae209d2",
+    "url": "/static/media/fg.8c4596ac.png"
+  },
+  {
+    "revision": "76fe2b8771874002dbece489d4ce0d54",
+    "url": "/static/media/iPhone-11-PNG-Transparent-HD-Photo.76fe2b87.png"
+  },
+  {
+    "revision": "e31ad85a22d256126b3186ed343e8e08",
+    "url": "/static/media/pc-gamer-transparent-png-clipart-free-download-ywd-pc-gamer-png-1000_600.e31ad85a.png"
+  },
+  {
+    "revision": "7157ce1a6fd5835f3cdaa251b2086311",
+    "url": "/static/media/pe-uhdtv-nu8000-un75nu8000gxpe-frontblack-113917484.7157ce1a.webp"
+  },
+  {
+    "revision": "f05c81d8bbc07d1b1b4c5ef11012d592",
+    "url": "/static/media/scarlet-red.f05c81d8.png"
+  }
+]);
